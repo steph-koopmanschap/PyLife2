3cc435d82d400b11aac17f33535c66c03b9e567c
steph-koopmanschap <stephkoopmanschap@protonmail.com>
2023-11-28 10:27:09 +0800

    bugfixes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import json
 import random
 import pygame
+import numpy as np
 #from pygame.math import Vector2
 from constants import COLORS, FPS, WINDOW_WIDTH, WINDOW_HEIGHT
 from models import all_sprites, species
@@ -9,8 +10,8 @@ from generate_world import generate_world
 from utils import update_tracker, log_tracker
 from globals import APP
 
+rng = np.random.default_rng() # Numpy's random number generator
 pygame.init()
-
 # Create the program window
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("PyLife2")
@@ -20,6 +21,27 @@ def clear_screen():
     window.fill(COLORS["BLACK"])
     #window.fill((4, 217, 255)) # Light blue
     #window.fill((255, 255, 255)) # White
+    
+def purge(purge_percentage):    
+    # Check which species is the most abundant
+    organism_counts = {}
+    for specie in species:
+        organism_counts[specie] = (APP['tracker'][f"total_{specie}"]) #specie.replace("total_", "")
+    most_abudant_species = max(organism_counts, key=organism_counts.get)
+    print(f"Most abundant species: {most_abudant_species} count: {organism_counts[most_abudant_species]}")
+    all_organisms_most_abundant = []
+    for organism in all_sprites:
+        if organism.params["species"] == most_abudant_species:
+            all_organisms_most_abundant.append(organism)
+    # Randomize the organisms to be deleted
+    rng.shuffle(all_organisms_most_abundant)
+    # only get a percentage of organisms
+    organisms_to_delete = all_organisms_most_abundant[:int(len(all_organisms_most_abundant) * purge_percentage)]
+    for organism in organisms_to_delete:
+        organism.destroy()
+        #organism_to_delete.kill()
+    print(f"Done deleting {len(organisms_to_delete)} ({purge_percentage*100}%) of {most_abudant_species}")
+    
 
 def handle_input(event):
     if event.type == pygame.QUIT:
@@ -71,22 +93,8 @@ def main_loop():
         # If the frame rate drops we will kill off purge_percentage of the most abudant species
         if current_fps < framerate_purge_level and pygame.time.get_ticks() - time_since_last_purge > purge_rate:
             print(f"Framerate dropping... Current FPS: {current_fps} ... Deleting {purge_percentage*100}% of the most abundant species.")
-            organism_counts = {}
-            for specie in species:
-                organism_counts[specie] = (APP['tracker'][f"total_{specie}"]) #specie.replace("total_", "")
-            most_abudant_species = max(organism_counts, key=organism_counts.get)
-            print(f"Most abundant species: {most_abudant_species} count: {organism_counts[most_abudant_species]}")
-            all_organisms_most_abundant = []
-            for organism in all_sprites:
-                if organism.params["species"] == most_abudant_species:
-                    all_organisms_most_abundant.append(organism)
-            number_of_organisms_to_delete = int(len(all_organisms_most_abundant) * purge_percentage)
-            for _ in range(number_of_organisms_to_delete):
-                organism_to_delete = random.choice(all_organisms_most_abundant)
-                organism_to_delete.destroy()
-                organism_to_delete.kill()
+            purge(purge_percentage)
             time_since_last_purge = pygame.time.get_ticks()
-            print(f"Done deleting {number_of_organisms_to_delete} ({purge_percentage*100}%) of {most_abudant_species}")
                 
         # Log statistics to file every 5 seconds
         elapsed_time = pygame.time.get_ticks() - time_since_last_logging
